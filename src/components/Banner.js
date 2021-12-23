2cc1124a831758262b4c9dc2228e89cc34626998
Ailenrosi <rorosiailu@gmailcom>
2021-12-23 15:14:29 -0300

    aplicacion con react

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Baner = () => {
+    return (
+        <section className="banner">
+            <h1 className="titulo">Tenemos lo mejor para ti...</h1>
+        </section>
+    );
+}
+
+export default Baner;
